07369384e478df361fd430e1bab73b9d1f3ccd61
Dan Ivy <danivy4@gmail.com>
2018-08-19 22:46:13 -0500

    Fix App component test

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,21 @@
 module.exports = {
-  'roots': [
-    '<rootDir>/src'
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
   ],
-  'transform': {
-    '.*\.tsx?$': 'ts-jest'
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js"
   },
-  'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  'moduleFileExtensions': [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node'
-  ]
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 }
