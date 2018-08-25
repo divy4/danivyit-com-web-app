a94fdef6f12ec6cf64f6c1c3529e84b3c79ffda4
Dan Ivy <danivy4@gmail.com>
2018-08-25 15:09:13 -0500

    Add footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,9 @@ export class Footer extends React.Component<IFooterProps> {
   render(): JSX.Element {
     return (
       <div className={styles.footer}>
+        <h4>
+          This React app is written in typescript, packaged by webpack, and deployed with a Docker swarm of Raspberry Pis.
+        </h4>
       </div>
     )
   }
