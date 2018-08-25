e0ef3a5299f213af177267b06c83bb3b5713b38d
Dan Ivy <danivy4@gmail.com>
2018-08-25 12:02:05 -0500

    Cleanup Header.test.tsx

--- src/__tests__/components/App.test.tsx
+++ src/__tests__/components/App.test.tsx
@@ -7,21 +7,21 @@ import { App } from '../../components/App';
 enzyme.configure({ adapter: new Adapter() });
 
 describe('<App>', () => {
-    let wrapper: any;
+  let wrapper: any;
 
-    beforeAll(() => {
-        wrapper = enzyme.shallow(<App/>);
-    });
+  beforeAll(() => {
+    wrapper = enzyme.shallow(<App/>);
+  });
 
-    test('renders a <Header>', () => {
-        expect(wrapper.find('Header')).toHaveLength(1);
-    });
+  test('renders a <Header>', () => {
+    expect(wrapper.find('Header')).toHaveLength(1);
+  });
 
-    test('renders a <Body>', () => {
-        expect(wrapper.find('Body')).toHaveLength(1);
-    });
+  test('renders a <Body>', () => {
+    expect(wrapper.find('Body')).toHaveLength(1);
+  });
 
-    test('renders a <Footer>', () => {
-        expect(wrapper.find('Footer')).toHaveLength(1);
-    });
+  test('renders a <Footer>', () => {
+    expect(wrapper.find('Footer')).toHaveLength(1);
+  });
 });
