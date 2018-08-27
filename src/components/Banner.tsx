50c94f3c3790b6d99b0492abd82e5e3865cac2da
Dan Ivy <danivy4@gmail.com>
2018-08-26 21:41:31 -0500

    Add Banner and BannerTitle

---
+++ src/components/Banner.tsx
@@ -0,0 +1,18 @@
+import * as styles from '../styles.css';
+import * as React from 'react';
+
+export { BannerTitle, IBannerTitleProps } from './BannerTitle';
+
+export interface IBannerProps {
+  src: any;
+}
+
+export class Banner extends React.Component<IBannerProps> {
+  render(): JSX.Element {
+    return (
+      <div className={styles.banner} style={{backgroundImage: `url(${this.props.src})`}}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
