4c1a648339d8d200c1ff4f1209a8112a28f0513a
Dan Ivy <danivy4@gmail.com>
2018-09-25 18:57:52 -0500

    Add className attribute to components (Fixes #20)

--- src/components/BannerTitle.tsx
+++ src/components/BannerTitle.tsx
@@ -1,14 +1,16 @@
+import classnames from 'classnames';
 import * as styles from '../styles.css';
 import * as React from 'react';
 
 export interface IBannerTitleProps {
   children: string;
+  className?: string;
 }
 
 export class BannerTitle extends React.Component<IBannerTitleProps> {
   render(): JSX.Element {
     return (
-      <h1 className={styles.bannerTitle}>
+      <h1 className={classnames(styles.bannerTitle, this.props.className)}>
         {this.props.children}
       </h1>
     );
